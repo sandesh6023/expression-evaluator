0b3bbf161ef8a7e6c5b63073620a56b5d057849e
sandesh6023 <sandy6023@gmail.com>
2014-01-22 18:39:00 +0530

    changes made to release notes

--- src/sandeshm/expeval/lib/expressionEvaluator.java
+++ src/sandeshm/expeval/lib/expressionEvaluator.java
@@ -1,7 +1,7 @@
 package sandeshm.expeval.lib;
 
-public class expressionEvaluator {
 
+public class expressionEvaluator {
     public int evaluateExpression(String expression) {
         String[] values = expression.split(" ");
         Operations operate = new Operations();
@@ -9,8 +9,9 @@ public class expressionEvaluator {
         int operand1 = Integer.parseInt(values[0]);
         int operand2 = Integer.parseInt(values[2]);
         String operator = values[1];
+
         if (operator.equals("+")) {
-           return result = operate.add(operand1, operand2);
+            return result = operate.add(operand1, operand2);
         }
         if (operator.equals("-")) {
             result = operate.subtract(operand1, operand2);
